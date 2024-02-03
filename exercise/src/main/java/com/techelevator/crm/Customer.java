5d9d1e24f4e5584bb3b062f9043ebf71b0966bd7
Alison Hall <alisonhall422@gmail.com>
2024-02-03 12:45:33 -0600

    java-gray-week3-team0

---
+++ exercise/src/main/java/com/techelevator/crm/Customer.java
@@ -0,0 +1,37 @@
+package com.techelevator.crm;
+
+import com.techelevator.Billable;
+import com.techelevator.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends Person implements Billable {
+    private String phoneNumber;
+    private List<Pet> pets = new ArrayList<>();
+
+    public Customer(String firstName, String lastName, String phoneNumber){
+        super (firstName, lastName);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer(String firstName, String lastName){
+        this(firstName, lastName, "");
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public List<Pet> getPets(){
+        return pets;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPets(List pets){
+        this.pets = pets;
+    }
+}
